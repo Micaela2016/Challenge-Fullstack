0d0c4b15a507f744c84355049cb7deaf48e42650
Micaela2016 <miqa.orellano@gmail.com>
2021-10-25 12:48:38 -0300

    Subida del proyecto inicial

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import "./styles.scss";
+import logo from "../../assets/logo.png";
+
+const Header = () => {
+    return (
+        <header>
+            <img src={logo} alt="Logo de balance"/>
+        </header>
+    );
+};
+export default Header;
