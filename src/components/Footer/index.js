f3ea12f57e593665c6cf818ea197cf34dc8d96dd
Micaela2016 <miqa.orellano@gmail.com>
2021-10-26 14:46:21 -0300

    Creacion de footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+const Header = () => {
+    return (
+        <footer>
+            <p>Todos los derechos reservados</p>
+            <a href="https://www.linkedin.com/in/micaela-orellano/" target="_blank" rel="noopener noreferrer">Micaela Orellano</a>
+        </footer>
+    );
+};
+export default Header;
